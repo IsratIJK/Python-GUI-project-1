ab8b6083741d6c0092175d7364a434cdf53fc74b
IsratIJK <iptu.jahan.amin@gmail.com>
2021-01-26 12:43:53 +0600

    Function added

---
+++ Function.py
@@ -0,0 +1,14 @@
+from tkinter import *
+MyGui = Tk()
+def hello():
+    L=Label(text='enter', fg='yellow',bg='orange',font=10).pack()
+def delete():
+    L2=Label(text='delete', fg='orange',bg='yellow',font=10).pack()
+MyGui.geometry('500x500+300+200')
+MyGui.title('Hello World')
+MyLabel=Label(text='Welcome',fg='#646060',bg='#F87217',font=10).pack()
+MyButton1=Button(text='Enter',fg='gray',bg='plum',command=hello,font=20).pack()
+MyButton2=Button(text='Delete',fg='gray',bg='plum',font=20).pack()
+
+
+MyGui.mainloop()

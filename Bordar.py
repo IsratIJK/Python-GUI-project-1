7f1d670a01665a9fa4a07874a7f2fd7565bd0e8a
IsratIJK <iptu.jahan.amin@gmail.com>
2021-01-26 12:09:50 +0600

    procedure of adding image file

--- Bordar.py
+++ Bordar.py
@@ -2,7 +2,8 @@ from tkinter import *
 window=Tk()
 window.geometry('500x500+150+300')
 window.title('Hello! This is my first project')
-b=Button(text='Hello! This is my first project',width=30,height=24,bg="blue",fg="white",activebackground="red",activeforeground="pink",bd=30,cursor="heart").grid()
+photo=PhotoImage(file=('social-whatsapp-circle-512.png'))
+b=Button(text='Hello! This is my first project',width=30,height=24,bg="blue",fg="white",activebackground="red",activeforeground="pink",bd=30,cursor="heart",underline=4,state=NORMAL,image=photo,highlightthickness=200).grid()
 l=Label(text='Hello! This is my first project',fg='red',bg='blue').grid(row=1,column=1)
 # we can't use grid() and pack() at a time.
 window.mainloop()
